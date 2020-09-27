357a6e5860c900b0209640834e43ce86661edeca
letrananhduc <letrananhduc0311@gmail.com>
2020-09-27 17:07:20 +0700

    Add initial section for Hello Duc

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header.js'
+import { HelloDuc } from './components/HelloDuc.js'
 import { Experiences } from './components/Experiences.js'
 import { Projects } from './components/Projects.js'
 import { Skills } from './components/Skills.js'
@@ -10,12 +11,13 @@ import { Blogs } from './components/Blogs.js'
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Experiences m/>
+      {/* <Header /> */}
+      <HelloDuc />
+      {/* <Experiences />
       <Projects />
       <Skills /> 
       <Blogs />
-      <Contact />
+      <Contact /> */}
     </div>
   );
 }
---
+++ client/src/components/HelloDuc.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HelloDuc.css'
+
+export function HelloDuc() {
+  return (
+    <div className="HelloDuc">
+      <p className="DucName">Le Tran Anh Duc</p>
+      <p className="DucTitle">Interactive resume</p>
+    </div>
+  )
+}
+
