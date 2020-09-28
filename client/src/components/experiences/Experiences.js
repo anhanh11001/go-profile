40876d09b77cbc3a9f4d988a232d4b65f7398c17
letrananhduc <letrananhduc0311@gmail.com>
2020-09-28 23:27:41 +0700

    Set up simple experiences section

--- client/src/components/experiences/Experiences.js
+++ client/src/components/experiences/Experiences.js
@@ -1,9 +1,59 @@
 import React from 'react';
+import './Experiences.css'
+import "../common/Common.css"
+import "../common/Common.js"
+import { LOREM } from '../common/Common.js';
 
-export function Experiences() {
+export class Experiences extends React.Component {
+
+  constructor(props) {
+    super(props); 
+    this.state = {
+
+    }
+  }
+
+  render() {
     return (
       <div className="Experiences" id="Experiences">
-        <h1>This is the experiences</h1>
+        <div className="container">
+          <text className="container_title">Experiences</text>
+          <text className="container_subtitle">Some deep quote about experiences</text>
+          <Experience />
+        </div>
       </div>
     )
   }
+}
+
+// TODO: This can just be a single function
+class Experience extends React.Component {
+
+  constructor(props) {
+    super(props); 
+    this.state = {
+        organization:"Organization Name",
+        organizationUrl:"Organization.url",
+        role:"Software Developer",
+        description:LOREM,
+        type:"Fulltime",
+        time:"Sometime - Present"
+    }
+  }
+
+  render() {
+    return (
+      <div className="Experience">
+        <div className="Experience_Main">
+          <text className="Experience_Org">{this.state.organization}</text>
+          <text className="Experience_Time">{this.state.time}</text>
+        </div>
+        <div className="Experience_Details">
+          <text className="Experience_Title">{this.state.type} - {this.state.role}</text>
+          <text>{this.state.description}</text>
+          <text>{this.organizationUrl}</text>
+        </div>
+      </div>
+    )
+  }
+}
--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "./Profile.css"
 import "../common/Common.css"
+import {LOREM} from "../common/Common.js"
 
-const lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
 export class Profile extends React.Component {
 
@@ -11,7 +11,7 @@ export class Profile extends React.Component {
     this.state = {
       name: "Le Tran Anh Duc",
       shortDescription: "Technology Enthusiast",
-      aboutMe: lorem,
+      aboutMe: LOREM,
       imageUrl: "res/images/profile.jpg",
       age: 20,
       location: "Remote"
@@ -22,21 +22,23 @@ export class Profile extends React.Component {
     return (
       <div className="Profile" id="Profile">
         <div className="container Profile_Container">
-          <text>Profile</text>
-          <text>{this.state.shortDescription}</text>
-          <div className="Profile_Detail">
-            <text>About Me</text>
-            <text>{this.state.aboutMe}</text>
-          </div>
-          <img className="round_img Profile_Img" src={this.state.imageUrl}/>
-          <div className="Profile_Detail">
-            <text>Details</text>
-            <text><b>Name:</b></text>
-            <text>{this.state.name}</text>
-            <text><b>Age:</b></text>
-            <text>{this.state.age}</text>
-            <text><b>Location:</b></text>
-            <text>{this.state.location}</text>
+          <text className="container_title">Profile</text>
+          <text className="container_subtitle">{this.state.shortDescription}</text>
+          <div>
+            <div className="Profile_Details">
+              <text>About Me</text>
+              <text>{this.state.aboutMe}</text>
+            </div>
+            <img className="round_img Profile_Img" src={this.state.imageUrl} alt="My Profile"/>
+            <div className="Profile_Details">
+              <text>Details</text>
+              <text><b>Name:</b></text>
+              <text>{this.state.name}</text>
+              <text><b>Age:</b></text>
+              <text>{this.state.age}</text>
+              <text><b>Location:</b></text>
+              <text>{this.state.location}</text>
+            </div>
           </div>
         </div>
       </div>
